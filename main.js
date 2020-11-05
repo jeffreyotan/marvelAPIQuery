f008d4ff150e15297279b4fff09f9bc068791503
jeffreyotan <jeffreyotan@yahoo.com>
2020-11-05 16:04:31 +0800

    Updated landing page to have search and pagination functionality

--- main.js
+++ main.js
@@ -10,32 +10,53 @@ const md5Hash = require('crypto-js/md5');
 const PORT = parseInt(process.argv[2]) || parseInt(process.env.PORT) || 3000;
 
 // define some global constants
-const QUERYLIMIT = 20;
+const QUERYLIMIT = parseInt(process.env.DB_QUERY_LIMIT) || 20;
 
 // retrieve the API_KEY needed to run the queries
 const PUB_API_KEY = process.env.PUB_API_KEY || "";
 const PRI_API_KEY = process.env.PRI_API_KEY || "";
 const baseUrl = "https://gateway.marvel.com:443";
 const characterUrl = "/v1/public/characters";
+const comicsUrl = "/v1/public/comics";
 
 let timeStamp = "1"; // alternatively, let timeStamp = (new Date()).getTime();
 // const queryString = "https://gateway.marvel.com:443/v1/public/characters?ts=" + timeStamp + "&apikey=" + PUB_API_KEY + "&hash=" + md5Hash(timeStamp + PRI_API_KEY + PUB_API_KEY);
 
-const generateQuery = (mainUrl) => {
+// create query strings using closure concept
+const generateListQuery = (mainUrl, limit = QUERYLIMIT, offset = 0) => {
     const newQuery = (ts, public_key, private_key) => {
         // const query = mainUrl + "?ts=" + ts + "&apikey=" + public_key + "&hash=" + md5Hash(ts + PRI_API_KEY + PUB_API_KEY);
         const query = withQuery(
             mainUrl, {
                 ts: ts,
                 apikey: public_key,
-                hash: md5Hash(ts + PRI_API_KEY + PUB_API_KEY).toString()
+                hash: md5Hash(ts + PRI_API_KEY + PUB_API_KEY).toString(),
+                limit: limit,
+                offset: offset
             }
         );
         return query;
     }
     return newQuery;
 }
-const Marvel_Query_Character_List = generateQuery(baseUrl + characterUrl);
+
+const generateSpecificQuery = (mainUrl, searchTerm, limit = QUERYLIMIT, offset = 0) => {
+    const newQuery = (ts, public_key, private_key) => {
+        // const query = mainUrl + "?ts=" + ts + "&apikey=" + public_key + "&hash=" + md5Hash(ts + PRI_API_KEY + PUB_API_KEY);
+        const query = withQuery(
+            mainUrl, {
+                ts: ts,
+                apikey: public_key,
+                hash: md5Hash(ts + PRI_API_KEY + PUB_API_KEY).toString(),
+                nameStartsWith: searchTerm,
+                limit: limit,
+                offset: offset
+            }
+        );
+        return query;
+    }
+    return newQuery;
+}
 
 // create instance of the express server
 const app = express();
@@ -46,8 +67,31 @@ app.set('view engine', 'hbs');
 
 // set up the routes using middlewares
 app.get('/', async (req, res, next) => {
+    const searchTerm = req.query['charSearch'];
+    const btnPressed = req.query['btnPressed'];
+    let currOffset = parseInt(req.query['offset']) || 0;
+
+    if(btnPressed === "next") {
+        currOffset = currOffset + QUERYLIMIT;
+    } else if (btnPressed == "prev") {
+        currOffset = Math.max(0, currOffset - QUERYLIMIT);
+    }
+
+    // normal query string if we hard code the information
     // const results = await fetch(queryString);
-    const results = await fetch(Marvel_Query_Character_List(timeStamp, PUB_API_KEY, PRI_API_KEY));
+
+    // use query string.. practice using closure
+    let results;
+    let search = "";
+
+    if(searchTerm) {
+        search = searchTerm;
+        const Marvel_Query_Character = generateSpecificQuery(baseUrl + characterUrl, searchTerm, QUERYLIMIT, currOffset);
+        results = await fetch(Marvel_Query_Character(timeStamp, PUB_API_KEY, PRI_API_KEY));
+    } else {
+        const Marvel_Query_Character_List = generateListQuery(baseUrl + characterUrl, QUERYLIMIT, currOffset);
+        results = await fetch(Marvel_Query_Character_List(timeStamp, PUB_API_KEY, PRI_API_KEY));
+    }
 
     try {
         const valReturned = await results.json();
@@ -58,7 +102,7 @@ app.get('/', async (req, res, next) => {
         console.info("==> character obtained: ", valReturned.data.results[0]);
 
         res.status(200).type('text/html');
-        res.render('index', { character: valReturned.data.results, attribution: valReturned.attributionHTML });
+        res.render('index', { character: valReturned.data.results, offset: currOffset, charSearch: search, attribution: valReturned.attributionHTML });
     } catch (error) {
         console.error('==> An internal server error occurred');
         res.status(500).type('text/html');
